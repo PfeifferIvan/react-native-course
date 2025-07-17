9e5633c8fbbd124ae8b515262066688696c3daeb
PfeifferIvan <ivanpfeiffer12@gmail.com>
2025-07-17 14:42:23 -0600

    styling app

--- GAMEAPP/components/PrimaryButton.js
+++ GAMEAPP/components/PrimaryButton.js
@@ -1,11 +1,41 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 function PrimaryButton(props) {
+  function pressHandler() {
+    console.log("Pressed");
+  }
+
   return (
-    <View>
-      <Text>{props.children}</Text>
+    <View style={styles.buttonInnerContainer}>
+      <Pressable
+        onPress={pressHandler}
+        android_ripple={{ color: "#640233" }}
+        style={({ pressed }) => (pressed ? styles.pressed : "")}
+      >
+        <Text style={styles.buttonText}>{props.children}</Text>
+      </Pressable>
     </View>
   );
 }
 
 export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    borderRadius: 28,
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+
+  pressed: {
+    opacity: 0.75,
+  },
+});
--- GAMEAPP/screens/StartGameScreen.js
+++ GAMEAPP/screens/StartGameScreen.js
@@ -4,9 +4,15 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <TextInput style={styles.input} maxLength={2} keyboardType="number-pad" />
+      <View style={styles.buttonsContainers}>
+        <View style={styles.buttonContainers}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainers}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
@@ -16,14 +22,34 @@ export default StartGameScreen;
 const styles = StyleSheet.create({
   inputContainer: {
     padding: 16,
+    alignItems: "center",
     marginTop: 100,
     marginHorizontal: 24,
     borderRadius: 8,
-    backgroundColor: "#72063c",
+    backgroundColor: "#3b021f",
     elevation: 4, //Shadow for android.
     shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.5,
+  },
+
+  input: {
+    height: 50,
+    width: 60,
+    fontSize: 32,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 2,
+    color: "#ddb52f",
+    marginVertical: 8,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+
+  buttonsContainers: {
+    flexDirection: "row",
+  },
+  buttonContainers: {
+    flex: 1,
   },
 });
