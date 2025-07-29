6b144eecc11dd7ca6c4e869a049594a1352f2ec8
PfeifferIvan <ivanpfeiffer12@gmail.com>
2025-07-29 11:29:54 -0600

    starting screen

---
+++ Meals App/components/CategoryGridTile.js
@@ -0,0 +1,52 @@
+import { Pressable, Text, View, StyleSheet, Platform } from "react-native";
+
+export default function CategoryGridTile({ title, color }) {
+  return (
+    <View style={styles.gridItem}>
+      <Pressable
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+        android_ripple={{ color: "#ccc" }}
+      >
+        <View style={[styles.innerContainer, , { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  gridItem: {
+    margin: 16,
+    height: 150,
+    width: 150,
+    borderRadius: 75,
+    elevation: 4,
+    backgroundColor: "white",
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "",
+  },
+  innerContainer: {
+    flex: 1,
+    padding: 16,
+    borderRadius: 75,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    flex: 1,
+  },
+  buttonPressed: {
+    opacity: 0.5,
+  },
+  title: {
+    fontWeight: "bold",
+    fontSize: 15,
+  },
+});
